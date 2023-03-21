a6c0e314c3e4cac7e6514c8cf84f722f95796852
Dmytro Holombytsia <iqbitsak@gmail.com>
2023-03-21 20:44:44 +0200

    create branch

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -1,12 +1,15 @@
 import { Box } from 'components/common/shared.styled';
 import { PetsTitle, AddPetBtn } from './PetsData.styled';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
 
 export const PetsData = () => {
   return (
     <Box>
       <Box>
         <PetsTitle>My pets</PetsTitle>
-        <AddPetBtn>Add pet</AddPetBtn>
+        <AddPetBtn>
+          Add pet <BsFillPlusCircleFill />
+        </AddPetBtn>
       </Box>
       {/* insert UserPetsList component */}
     </Box>
