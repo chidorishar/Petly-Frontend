16aedfd2a1482771391daa0e0d53cfc4b6539bab
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-22 15:13:57 +0200

    Applied global styles to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,15 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { store } from 'redux/store';
 import { useLazyRefreshUserQuery } from 'redux/slices/usersAPISlice';
+import { ROUTES } from 'utils/appKeys';
 
 import SharedLayout from './SharedLayout/SharedLayout';
 import { RestrictedRoute } from './ProtectedRoute';
 // import { PrivateRoute } from './PrivateRoute';
-
-import { ROUTES } from 'utils/appKeys';
-
 // TODO: Add lazy loading.
 import RegisterPage from 'pages/Register/RegisterPage';
+
+import { GlobalStyle } from 'utils';
 
 export const App = () => {
   const [refreshUser, { isLoading: isRefreshingUserData }] =
@@ -24,25 +24,28 @@ export const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        {isRefreshingUserData ? (
-          <Route index element={<p>Retrieving data...</p>} />
-        ) : (
-          <>
-            {/* HOMEPAGE */}
-            <Route
-              index
-              element={<RestrictedRoute redirectTo="/" component={<></>} />}
-            />
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          {isRefreshingUserData ? (
+            <Route index element={<p>Retrieving data...</p>} />
+          ) : (
+            <>
+              {/* HOMEPAGE */}
+              <Route
+                index
+                element={<RestrictedRoute redirectTo="/" component={<></>} />}
+              />
 
-            {/* ⏬ WRITE your PAGES below this comment ⏬*/}
-            <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-            <Route path="*" element={<></>} />
-          </>
-        )}
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+              {/* ⏬ WRITE your PAGES below this comment ⏬*/}
+              <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+              <Route path="*" element={<></>} />
+            </>
+          )}
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
