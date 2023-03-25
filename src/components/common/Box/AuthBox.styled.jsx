730fd7293312d80be14349e5495d1ace74019015
SuhakMV <sugamish@gmail.com>
2023-03-25 18:28:08 +0200

    fix loginform styles

--- src/components/common/Box/AuthBox.styled.jsx
+++ src/components/common/Box/AuthBox.styled.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import desktop from '../../../images/auth/desktop.png';
 import tablet from '../../../images/auth/tablet.png';
 import mobile from '../../../images/auth/mobile.png';
-
+import desktop2x from '../../../images/auth/desktop@2x.png';
+import tablet2x from '../../../images/auth/tablet@2x.png';
+import mobile2x from '../../../images/auth/mobile@2x.png';
 
 export const AuthBox = styled.div`
   height: 100%;
@@ -12,20 +14,35 @@ export const AuthBox = styled.div`
   background-size: contain;
 
   @media ${p => p.theme.breakpoints.tablet.desktop} {
-    padding-top: 81px;
-    padding-bottom: 134px;
+    padding-top: 91px;
+    padding-bottom: 124px;
     background-image: url(${desktop});
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${desktop2x});
+    }
   }
 
   @media ${p => p.theme.breakpoints.tablet.media} and (max-width: 1279px) {
     padding-top: 204px;
     padding-bottom: 260px;
     background-image: url(${tablet});
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${tablet2x});
+    }
   }
 
   @media (max-width: 767px) {
     padding-top: 42px;
     padding-bottom: 151px;
     background-image: url(${mobile});
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${mobile2x});
+    }
   }
 `;
