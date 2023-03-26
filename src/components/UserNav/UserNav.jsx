d1c70adf7f2308749117ce51b8250e6f6804f2f5
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-03-26 17:03:12 +0300

    fix: Correct account route navigation; add current user backend endpoint.

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,10 +1,11 @@
 import { UserNavBox, UserNavLink, UserNavIcon } from './UserNav.styled';
 import AccountIcon from './account.svg';
+import { ROUTES } from 'utils/appKeys';
 
 export const UserNav = () => {
   return (
     <UserNavBox>
-      <UserNavLink to="/user">
+      <UserNavLink to={ROUTES.PROFILE}>
         <UserNavIcon src={AccountIcon} alt="account" />
         Account
       </UserNavLink>
