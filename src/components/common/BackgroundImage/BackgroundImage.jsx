f295fc628581fddd15f9e9fa5088ff0e04092fe4
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-27 14:30:06 +0300

    Added common Background image component

---
+++ src/components/common/BackgroundImage/BackgroundImage.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const BackgroundImage = ({
+  srcSetWebp,
+  srcSetOldTypes,
+  placeholderImg,
+  typeOldTypes = 'image/jpeg',
+  typeWebp = 'image/webp',
+}) => {
+  return (
+    //     <picture>
+    //   <source srcset="photo.webp 1x, photo@2x.webp 2x" type="image/webp" />
+    //   <source srcset="photo.jpg 1x, photo@2x.jpg 2x" type="image/jpeg" />
+    //   <img src="photo.jpg" alt="Кот" />
+    // </picture>
+    <picture>
+      <source srcSet={srcSetWebp} type={typeWebp} />
+      <source srcSet={srcSetOldTypes} type={typeOldTypes} />
+      <img src={placeholderImg} />
+    </picture>
+  );
+};
+
+export default BackgroundImage;
+
+BackgroundImage.propTypes = {
+  srcSetWebp: PropTypes.string.isRequired,
+  srcSetOldTypes: PropTypes.string.isRequired,
+  placeholderImg: PropTypes.string.isRequired,
+  typeOldTypes: PropTypes.string,
+  typeWebp: PropTypes.string,
+};
---
+++ src/components/common/BackgroundImage/BackgroundImage.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import BackgroundImage from './BackgroundImage';
+
+export const BackgroundImageStyled = styled(BackgroundImage)`
+  position: relative;
+  z-index: -1;
+`;
