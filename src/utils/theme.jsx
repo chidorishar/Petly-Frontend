a02be1f041bcd331f86b5d1ebb5ed185ca36b2f8
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-27 18:54:21 +0300

    Fixed registration page

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -76,6 +76,7 @@ export const theme = {
     },
     tablet: {
       media: '(min-width: 768px) and (max-width: 1279px)',
+      mediaFrom: '(min-width: 768px)',
       width: '768px',
     },
     desktop: {
