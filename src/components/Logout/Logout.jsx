4cf75a459e6f09092fde48c96c2f002854f8df40
Надія Ткач <nadiya77723@gmail.com>
2023-03-28 13:13:41 +0300

    add translate notices title

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,8 +1,10 @@
 import { LogOutBtn, LogoutIcon } from './Logout.styled';
 import { useLogoutUserMutation } from 'redux/slices/usersAPISlice';
+// import { useTranslation } from 'react-i18next';
 
 export const Logout = () => {
   const [logout] = useLogoutUserMutation();
+  // const { t } = useTranslation();
 
   return (
     <LogOutBtn onClick={logout}>
--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { PetsData } from 'components/PetsData/PetsData';
 import { UserData } from 'components/UserData/UserData';
+import { useTranslation } from 'react-i18next';
 
 import {
   UserPageSection,
@@ -31,11 +32,13 @@ const UserPage = () => {
     getUserData();
   }, []);
 
+  const { t } = useTranslation();
+
   return (
     <UserPageSection>
       <UserPageContainer>
         <UserInfoContainer>
-          <UserTitle>My information:</UserTitle>
+          <UserTitle>{t('user.info')}</UserTitle>
           {userData && (
             <UserWrapper>
               <UserData user={userData} onUserDataUpdated={onUserDataUpdated} />
