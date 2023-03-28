6feb105eddf17cc9f960df718aee2ba1041d7588
Надія Ткач <nadiya77723@gmail.com>
2023-03-28 11:54:18 +0300

    add translate validation loginSchema

--- src/utils/validations/loginSchema.js
+++ src/utils/validations/loginSchema.js
@@ -1,19 +1,20 @@
 import * as Yup from 'yup';
+import i18n from 'i18next';
 
 const loginschema = Yup.object().shape({
   email: Yup.string()
-    .required('Email is a required field')
-    .email('Enter a valid Email'),
+    .required(i18n.t('validation.requiredEmail'))
+    .email(i18n.t('validation.invalidEmail')),
   password: Yup.string()
-    .required('Password is a required field')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/[0-9]/, 'Password requires a number')
-    .matches(/[a-z]/, 'Password requires a lowercase letter')
-    .matches(/[A-Z]/, 'Password requires an uppercase letter')
-    .matches(/[^\w]/, 'Password requires a symbol'),
+    .required(i18n.t('validation.requiredPass'))
+    .min(8, i18n.t('validation.passMin'))
+    .matches(/[0-9]/, i18n.t('validation.passNumber'))
+    .matches(/[a-z]/, i18n.t('validation.passLowercase'))
+    .matches(/[A-Z]/, i18n.t('validation.passUppercase'))
+    .matches(/[^\w]/, i18n.t('validation.passSymbol')),
   confirm: Yup.string().oneOf(
     [Yup.ref('password'), null],
-    'Must match "password" field value'
+    i18n.t('validation.confirm')
   ),
 });
 
