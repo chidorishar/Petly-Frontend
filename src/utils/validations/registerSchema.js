091bcae618d13544959d60917cb552969de5a74b
SuhakMV <sugamish@gmail.com>
2023-03-29 14:00:39 +0300

    fix validation

--- src/utils/validations/registerSchema.js
+++ src/utils/validations/registerSchema.js
@@ -7,7 +7,7 @@ import * as Yup from 'yup';
 import { passwordSchema, emailSchema, phoneSchema } from './common';
 
 const locationRegexp =
-  /^([a-zA-Zа-яА-ЯІіЇїЄє]+){2}, ([a-zA-Zа-яА-ЯІіЇїЄє]+){2}$/;
+  /^([-'a-zA-Zа-яА-ЯІіЇїЄє\u0410-\u044F`]+){2}, ([-'a-zA-Zа-яА-ЯІіЇїЄє\u0410-\u044F`]+){2}$/;
 
 const registerSchema = Yup.object({
   email: emailSchema,
@@ -19,7 +19,7 @@ const registerSchema = Yup.object({
   name: Yup.string()
     .required('Name is required')
     .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
-    .max(40 , 'Please enter valid name'),
+    .max(40, 'Please enter valid name'),
   location: Yup.string()
     .matches(locationRegexp, 'Must be in format: City, Region')
     .required('City, region is required'),
