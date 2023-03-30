a03385f00924212781e313863eb39e94271529dc
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-30 22:15:25 +0300

    Added color transitions for localization buttons

--- src/components/common/LanguageSwitch/LanguageSwitch.styled.js
+++ src/components/common/LanguageSwitch/LanguageSwitch.styled.js
@@ -22,24 +22,39 @@ export const LanguageOption = styled.input`
   background-color: ${p => p.theme.colors.secondaryBackground};
   color: ${p => p.theme.colors.black};
   font-size: ${p => p.theme.fontSizes.xs};
-  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms;
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color ${p => p.theme.transitions.normal}, box-shadow 250ms;
 
   :first-child {
     margin-right: 10px;
   }
-  &:hover {
+
+  &:hover,
+  &:focus {
     font-weight: ${p => p.theme.fontWeights.logo};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    text-shadow: 0px 0px 6px rgba(255, 255, 255, 1);
+    text-shadow: 0px 0px 6px ${p => p.theme.colors.accent};
   }
+
   &:not(:checked) {
     color: ${p => p.theme.colors.heading};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.accent};
+    }
   }
 
   &:checked {
     background-color: ${p => p.theme.colors.accent};
     font-weight: ${p => p.theme.fontWeights.logo};
+
+    &:hover,
+    &:focus {
+      color: ${p => p.theme.colors.white};
+    }
   }
+
   &::before {
     content: attr(label);
     text-align: center;
