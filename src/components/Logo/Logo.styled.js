d072331f716090b5b80af2ca2bd4257218a13a01
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-30 22:24:07 +0300

    Added color transitions for site LOGO

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -6,6 +6,10 @@ export const LogoImg = styled(LogoSVGComp)`
   --logoPrimary: ${p => p.theme.colors.heading};
   --logoAccent: ${p => p.theme.colors.accent};
 
+  & > path {
+    transition: fill 1s ease-in-out;
+  }
+
   width: 82px;
   @media (min-width: 768px) {
     width: 94px;
