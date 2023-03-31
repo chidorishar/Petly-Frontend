6ecb92e28c84c0c23f47407aa285338d3acf12ce
Надія Ткач <nadiya77723@gmail.com>
2023-03-31 17:13:01 +0300

    add pading title in login

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -50,6 +50,7 @@ export const Title = styled.h2`
   transition: color ${({ theme }) => theme.transitions.normal};
 
   @media ${p => p.theme.breakpoints.mobile.media} {
+    padding-top: 10px;
     font-weight: ${p => p.theme.fontWeights.logo};
     font-size: ${p => p.theme.fontSizes.ml};
   }
