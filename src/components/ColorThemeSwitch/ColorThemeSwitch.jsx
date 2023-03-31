ac233fe7c134b0fde3d8c16bc83f0db2ca782f99
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-31 04:25:22 +0300

    Added theme hook

--- src/components/ColorThemeSwitch/ColorThemeSwitch.jsx
+++ src/components/ColorThemeSwitch/ColorThemeSwitch.jsx
@@ -6,10 +6,15 @@ import * as Styled from './ColorThemeSwitch.styled';
 
 export const ColorThemeSwitch = () => {
   const { themeMode, toggleTheme } = useAppTheme();
+  const isDarkMode = themeMode === THEME_MODES.DARK;
 
   return (
-    <Styled.ToggleButton onClick={toggleTheme} aria-label="Switch color theme">
-      {themeMode === THEME_MODES.LIGHT ? <CgSun /> : <CgMoon />}
+    <Styled.ToggleButton
+      onClick={toggleTheme}
+      $isDarkMode={isDarkMode}
+      aria-label="Switch color theme"
+    >
+      {isDarkMode ? <CgMoon /> : <CgSun />}
     </Styled.ToggleButton>
   );
 };
